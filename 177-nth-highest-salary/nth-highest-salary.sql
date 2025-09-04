b887c80ed018e94ef56d5c6fd8f741ed78ff63cd
Parag Aher <140876887+paragaher15@users.noreply.github.com>
2025-09-04 15:12:05 +0530

    Time: 426 ms (68.23%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+ SELECT DISTINCT salary
+FROM (SELECT salary,
+           DENSE_RANK() OVER (ORDER BY salary DESC) as rnk
+    FROM Employee
+) as subquery
+WHERE rnk = n
+
+  );
+END
