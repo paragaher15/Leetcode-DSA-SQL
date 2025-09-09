72931fe7a6a336422dbe5abcd97cbcdf03258541
Parag Aher <140876887+paragaher15@users.noreply.github.com>
2025-09-09 12:07:56 +0530

    Time: 747 ms (11.09%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,8 +1,14 @@
-
-
-SELECT name as Customers 
-FROM Customers
-WHERE id NOT IN (
-    SELECT customerId
-    FROM Orders
+SELECT name as Customers
+from  Customers 
+where id NOT IN(
+    select customerId from orders
 );
+
+
+
+-- SELECT name as Customers 
+-- FROM Customers
+-- WHERE id NOT IN (
+--     SELECT customerId
+--     FROM Orders
+-- );
