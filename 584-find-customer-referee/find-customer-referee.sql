be435bb8deabe19071ed730c27cdf764a036caf6
Parag Aher <140876887+paragaher15@users.noreply.github.com>
2025-09-12 17:01:40 +0530

    Time: 439 ms (87.79%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -5,5 +5,8 @@
 -- on c.id = c1.referee_id
 -- where c1.referee_id != 2 OR c1.referee_id IS NULL;
 
-select name from customer where 
-referee_id != 2 OR referee_id IS NULL;
+-- select name from customer where 
+-- referee_id != 2 OR referee_id IS NULL;
+
+select name from Customer
+where referee_id != 2 or referee_id IS NULL
