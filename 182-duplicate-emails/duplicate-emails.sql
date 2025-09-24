074c8d2d96c406cb182c69728a6d38db74f96eae
Parag Aher <140876887+paragaher15@users.noreply.github.com>
2025-09-24 13:43:15 +0530

    Time: 396 ms (28.91%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
-SELECT email AS Email
-from Person
-GROUP BY email
-Having count(*) > 1;
+-- SELECT email AS Email
+-- from Person
+-- GROUP BY email
+-- Having count(*) > 1;
+select email as Email
+from person
+group by email
+HAVING COUNT(*) > 1
