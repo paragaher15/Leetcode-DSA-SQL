457225a18f2826743adf2bac33d2052d526c7382
Parag Aher <140876887+paragaher15@users.noreply.github.com>
2025-09-24 13:13:17 +0530

    Time: 340 ms (34.57%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -3,7 +3,11 @@
 --     DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
 -- FROM Scores
 -- ORDER BY score DESC;
+-- select score,
+--  DENSE_RANK() OVER (ORDER BY Score DESC) As 'rank'
+-- FROM Scores
+-- ORDER BY score DESC;
+
 select score,
- DENSE_RANK() OVER (ORDER BY Score DESC) As 'rank'
-FROM Scores
-ORDER BY score DESC;
+DENSE_RANK () OVER (ORDER BY score DESC) as 'rank'
+from scores
