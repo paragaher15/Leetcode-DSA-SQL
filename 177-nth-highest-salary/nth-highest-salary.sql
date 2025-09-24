b9c963f0714d8314b016fb5aa3e924f9759b1f8b
Parag Aher <140876887+paragaher15@users.noreply.github.com>
2025-09-24 13:10:27 +0530

    Time: 901 ms (6.30%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,10 +1,15 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
-    SELECT DISTINCT salary 
-    FROM (
-        SELECT salary, 
-        DENSE_RANK() OVER (ORDER BY salary DESC) as rnk FROM Employee )
-         as subquery WHERE rnk = n
+      select DISTINCT salary
+      from (
+        select salary,
+      DENSE_RANK () OVER (ORDER BY salary DESC) as rnk
+      from Employee
+      )
+      as subquery
+      where rnk = N
+
+
   );
 END
