9fa0fe885806a13561c8c9f9be154ab9945df9cb
Parag Aher <140876887+paragaher15@users.noreply.github.com>
2025-09-24 14:15:50 +0530

    Time: 307 ms (93.08%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,11 +1,21 @@
 # Write your MySQL query statement below
+-- select
+-- CASE
+-- WHEN id = (select max(id) from Seat) and mod(id,2) = 1 THEN id
+-- WHEN mod(id,2) = 1 THEN id+1
+-- ELSE id-1
+-- END AS id,student
+-- from Seat
+-- order by id ASC
+
+
+
+
 select
-CASE
-WHEN id = (select max(id) from Seat) and mod(id,2) = 1 THEN id
+CASE 
+wHEN id = (select max(id) from seat) and mod(id,2) = 1 THEN id
 WHEN mod(id,2) = 1 THEN id+1
 ELSE id-1
-END AS id,student
-from Seat
+END as id,student
+from seat
 order by id ASC
-
-
