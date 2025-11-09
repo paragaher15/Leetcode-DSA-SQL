ef24cd0546502d11c43ebf81202620cf9412f750
Parag Aher <140876887+paragaher15@users.noreply.github.com>
2025-11-10 04:26:28 +0530

    Time: 11 ms (83.35%) | Memory: 20.7 MB (75.31%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,12 @@
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram = {}
+        for word in strs:
+            key = ''.join(sorted(word))
+            if key in anagram:
+                anagram[key].append(word)
+            else:
+                anagram[key] = [word]
+        return list(anagram.values())
