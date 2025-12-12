02034ee3c2bf1081deab3adacd4480cff347e679
Parag Aher <140876887+paragaher15@users.noreply.github.com>
2025-12-12 22:15:46 +0530

    Time: 12 ms (51.27%) | Memory: 17.8 MB (62.80%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,21 @@
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
-        
+        hash_map = {}
+        hash_map1 = {}
 
-  
+        for ch in s:
+            if ch in hash_map:
+                hash_map[ch] += 1
+            else:
+                hash_map[ch] = 1
+
+        for ch in t:
+            if ch in hash_map1:
+                hash_map1[ch] += 1
+            else:
+                hash_map1[ch] = 1
+
+        if hash_map == hash_map1:
+            return True
+        
+        return False
