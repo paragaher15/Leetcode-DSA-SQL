ca97eae242826d05a02dba737dddbd01a1047326
Parag Aher <140876887+paragaher15@users.noreply.github.com>
2025-12-20 08:48:42 +0530

    Time: 11 ms (17.62%) | Memory: 19.7 MB (42.62%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,6 +1,18 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        result = 0
+        hash_map = {}
+
         for num in nums:
-            result = result ^ num
-        return result
+            if num in hash_map:
+                hash_map[num] += 1
+            else:
+                hash_map[num] = 1
+
+        for key, value in hash_map.items():
+            if value == 1:
+                return key
+
+        # result = 0
+        # for num in nums:
+        #     result = result ^ num
+        # return result
